df0e5ded2f9ce646ad59baccd06c65bcc4999600
Francisco Bessa <edenilson.victoralves@outlook.com>
2023-04-26 20:19:41 -0300

    Added validation to input fields

--- src/components/InputDateForm.jsx
+++ src/components/InputDateForm.jsx
@@ -1,12 +1,17 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import arrowDown from './assets/icon-arrow.svg'
 import resetIcon from './assets/icon-reset.svg'
 import { BiReset } from "react-icons/bi";
+import Tooltip from "./Tooltip";
 
 const InputDateForm = () => {
     const [validDay, setValidDay] = useState(true)
     const [validMonth, setValidMonth] = useState(true)
     const [validYear, setValidYear] = useState(true)
+    const [showToolTip, setShowToolTip] = useState(false)
+    const dayRef = useRef(null)
+    const monthRef = useRef(null)
+    const yearRef = useRef(null)
 
     const thisDayIsValid = (day) => {
         if (day > 0 && day <= 31) {
@@ -34,10 +39,16 @@ const InputDateForm = () => {
         return false
     }
 
+    const clearInputFields = () => {
+        dayRef.current.value = ""
+        monthRef.current.value = ""
+        yearRef.current.value = ""
+    }
+
     const formSubmitHandler = (event) => {
         event.preventDefault()
 
-        const day = Number(event.target.day.value)
+        const day = event.target.day.value
         const month = event.target.month.value
         const year = event.target.year.value
 
@@ -62,20 +73,29 @@ const InputDateForm = () => {
     return (
         <div className="mt-[100px]">
             <form className="flex justify-center" onSubmit={formSubmitHandler}>
-                <BiReset className="mt-6 mr-2 text-[#694aff] hover:cursor-pointer" />
+                <div className="relative inline-block">
+                    {showToolTip && <Tooltip message="Clear fields" show={showToolTip} />}
+                    
+                    <BiReset
+                        className="mt-6 mr-2 text-[#694aff] hover:cursor-pointer"
+                        onClick={clearInputFields}
+                        onMouseEnter={() => setShowToolTip(true)}
+                        onMouseLeave={() => setShowToolTip(false)}
+                    />
+                </div>
                 <div className="flex flex-col">
                     <label htmlFor="day" className={`label-date text-gray-500`}>DAY</label>
-                    <input type="number" id="day" placeholder="DD" required className="input-date"></input>
+                    <input type="number" id="day" placeholder="DD" required className="input-date" ref={dayRef}></input>
                     {!validDay && <p className={`text-[8px] mt-1 text-red-500`}>Must be a valid day</p>}
                 </div>
                 <div className="flex flex-col">
                     <label htmlFor="month" className={`label-date text-gray-500 ml-3`}>MONTH</label>
-                    <input type="number" id="month" placeholder="MM" required className="input-date ml-3"></input>
+                    <input type="number" id="month" placeholder="MM" required className="input-date ml-3" ref={monthRef}></input>
                     {!validMonth && <p className={`text-[8px] mt-1 ml-3 text-red-500`}>Must be a valid month</p>}
                 </div>
                 <div className="flex flex-col">
                     <label htmlFor="year" className={`label-date text-gray-500 ml-3`}>YEAR</label>
-                    <input type="number" id="year" placeholder="YYYY" required className="input-date ml-3"></input>
+                    <input type="number" id="year" placeholder="YYYY" required className="input-date ml-3" ref={yearRef}></input>
                     {!validYear && <p className={`text-[8px] mt-1 ml-3 text-red-500`}>Must be a valid year</p>}
                 </div>
                 <button type="submit" className="rounded-full w-[45px] h-[45px] bg-[#694aff] text-white ml-6 mt-16 flex justify-center items-center hover:cursor-pointer">
